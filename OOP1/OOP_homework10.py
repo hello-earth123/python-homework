b81534210afc6559b7cea35a2b1a813d4dd0af17
hello-earth123 <rock7501@naver.com>
2025-07-30 17:59:43 +0900

    d

---
+++ OOP1/OOP_homework10.py
@@ -0,0 +1,11 @@
+# 아래 클래스를 수정하시오.
+class Person:
+    #생성자 메서드
+    def __init__(self):
+        pass
+
+
+
+person1 = Person("Alice", 25)
+person1.introduce()
+print(Person.number_of_people)
---
+++ OOP1/OOP_homework7.py
@@ -0,0 +1,23 @@
+# 아래 클래스를 수정하시오.
+class Shape:
+    #생성자 메서드
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    #인스턴스 메서드
+    def calculate_area(self):
+        return (self.width * self.height)
+
+    #인스턴스 메서드2
+    def calculate_perimeter(self):
+        return 2 * (self.width + self.height)
+    
+    def print_info(self):
+        print(f'Width: {self.width}')
+        print(f'Height: {self.height}')
+        print(f'Area: {self.calculate_area()}')
+        print(f'Perimeter: {self.calculate_perimeter()}')
+
+shape1 = Shape(5, 3)
+shape1.print_info()
